d1e146315288a59bfaa61ed9e7582b200da31625
AdrianBrown1 <adrian.angel.brown@gmail.com>
2017-01-17 21:14:53 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // write your code below!
+function happyHolidays() {
+
+  return "Happy Holidays!";
+
+}
+
+function happyHolidaysTo(name) {
+
+var greeting = "Happy holidays, " + name + "!"; 
+
+return greeting;
+
+}
+happyHolidaysTo("Adrian")
+
+function happyHolidayTo(holiday, name) {
+ 
+  var greeting = "Happy " + holiday + name; 
+
+  return greeting; 
+}
+
+function holidayCountdown(days, holiday) {
+
+  var countDown = "It's " + days + " days until " + holiday + "!";
+
+  return countDown; 
+}
+
